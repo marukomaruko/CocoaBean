a99d7829844db242cd25bb96182ccd06d998b63a
Zhao Shao Wan <zhaoshaowan@gmail.com>
2016-01-22 13:00:47 +0800

    Add generating cli task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,12 @@
 import gulp from 'gulp';
+import babel from 'gulp-babel';
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('cli', function() {
+  gulp.src('cli/**/*.js')
+      .pipe(babel({
+        presets: ['es2015', 'stage-0']
+      }))
+      .pipe(gulp.dest('cli-compiled'));
 });
+
+gulp.task('default', ['cli']);
