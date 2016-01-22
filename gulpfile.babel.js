73f53c631cfabf49cc77583ac03688173ed8840e
Zhao Shao Wan <zhaoshaowan@gmail.com>
2016-01-22 13:09:46 +0800

    Use mocha to test new cli code

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,12 +1,20 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
+import mocha from 'gulp-mocha';
 
 gulp.task('cli', function() {
-  gulp.src('cli/**/*.js')
-      .pipe(babel({
-        presets: ['es2015', 'stage-0']
-      }))
-      .pipe(gulp.dest('cli-compiled'));
+  return gulp
+    .src('cli/**/*.js')
+    .pipe(babel({
+      presets: ['es2015', 'stage-0']
+    }))
+    .pipe(gulp.dest('cli-compiled'));
 });
 
-gulp.task('default', ['cli']);
+gulp.task('test', () => {
+  return gulp
+    .src('cli-test/**/*.js')
+    .pipe(mocha({reporter: 'nyan', ui: 'tdd'}));
+});
+
+gulp.task('default', ['cli', 'test']);
